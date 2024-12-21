f7679d887424a6db8bb504d25e0c81518c20da18
Valentin <72377525+valentin-sys@users.noreply.github.com>
2024-12-21 17:32:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+// Start time for latency calculation
+$start_time = microtime(true);
+
+// Simulate some processing (optional)
+usleep(100000); // 100 milliseconds
+
+// Calculate latency
+$latency = microtime(true) - $start_time;
+
+// Get HTTP status
+$http_status = http_response_code();
+
 echo '<h1>Response Headers</h1>';
 echo '<pre>';
 print_r(getallheaders());
@@ -13,6 +25,8 @@ echo '<p>User Agent: ' . $_SERVER['HTTP_USER_AGENT'] . '</p>';
 echo '<h1>Server Information</h1>';
 echo '<p>Server Time: ' . date('Y-m-d H:i:s') . '</p>';
 echo '<p>Server Name: ' . $_SERVER['SERVER_NAME'] . '</p>';
+echo '<p>Latency: ' . round($latency * 1000, 2) . ' ms</p>';
+echo '<p>HTTP Status: ' . $http_status . '</p>';
 
 echo '<h1>Cookies</h1>';
 echo '<pre>';
