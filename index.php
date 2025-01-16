45a661b82926c8a908c6b13c7e57028add0ac602
Valentin <72377525+valentin-sys@users.noreply.github.com>
2025-01-16 12:40:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,12 +16,6 @@ echo '<pre>';
 print_r(getallheaders());
 echo '</pre>';
 
-echo '<h1>Request Information</h1>';
-echo '<p>Request Method: ' . $_SERVER['REQUEST_METHOD'] . '</p>';
-echo '<p>Request URL: ' . $_SERVER['REQUEST_URI'] . '</p>';
-echo '<p>Client IP: ' . $_SERVER['REMOTE_ADDR'] . '</p>';
-echo '<p>User Agent: ' . $_SERVER['HTTP_USER_AGENT'] . '</p>';
-
 echo '<h1>Server Information</h1>';
 echo '<p>Server Time: ' . date('Y-m-d H:i:s') . '</p>';
 echo '<p>Server Name: ' . $_SERVER['SERVER_NAME'] . '</p>';
